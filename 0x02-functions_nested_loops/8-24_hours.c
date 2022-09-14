47f3963d51e63772e28d83db3ac106d4c848caa1
Oluwatobi <towbeedaniels@gmail.com>
2022-09-13 20:18:11 -0700

    All tasks

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauer - func that prints every minute of the day of Jack Bauer, starting
+ * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
  *
  * from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  *
