181a575661202defc6331686666e8ba2e1701815
Oluwatobi <towbeedaniels@gmail.com>
2022-09-13 20:08:09 -0700

    All tasks

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,23 +3,17 @@
 
 
 /**
+ * add - Adds two integers.
  *
- *  * add - Adds two integers.
+ * @num1: The first integer to be added.
  *
- *   * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
  *
- *    * @num2: The second integer to be added.
+ * Return: The result of the addition.
  *
- *     *
- *
- *      * Return: The result of the addition.
- *
- *       */
+ */
 
 int add(int num1, int num2)
-
 {
-
-		return (num1 + num2);
-
+	return (num1 + num2);
 }
