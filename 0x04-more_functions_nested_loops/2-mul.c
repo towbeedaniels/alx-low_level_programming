ecb42e6d034e3526cea4918f652ef4d5f91089e0
Oluwatobi <towbeedaniels@gmail.com>
2022-09-15 18:30:42 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - retuens the results of the parameters
+ * @a: int type number
+ * @b: int type number
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
